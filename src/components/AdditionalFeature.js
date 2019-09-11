1f655ed6da2bf5d576fccdf1a2ed6d384528248b
zachary-peasley <zpman95@hotmail.com>
2019-09-11 15:57:33 -0500

    MVP

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import AddedFeature from './AddedFeature';
+
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button" onClick={() => addedFeature(props.feature)}>Add</button>
+      <button className="button" onClick={() => props.addFeature(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux';
+import { reducer } from './reducer';
+import { Provider } from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(reducer);
+console.log('store');
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}>
+<App /> 
+</Provider>, rootElement);
